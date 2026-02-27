0e5657f1680b5b02cb774ca459be10482ce46d96
annushkaskirko-source <annushkaskirko@gmail.com>
2026-02-27 12:23:32 +0200

    Update Player.cs

--- IC-o51_Skirko_Ann_08_02_2026/Models/Player.cs
+++ IC-o51_Skirko_Ann_08_02_2026/Models/Player.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,66 +6,34 @@ using System.Threading.Tasks;
 
 namespace IC_o51_Skirko_Ann_08_02_2026.Models
 {
-    // Клас гравця
+    // Модель гравця
     public class Player
     {
-        // Приватні поля (інкапсуляція)
-        private string _name;          // зберігає ім'я
-        private int _experience;       // зберігає досвід
-
-        public const int XP_PER_LEVEL = 100; // Скільки XP потрібно на 1 рівень
-
-        // Властивість Name з перевіркою
-        public string Name
-        {
-            get => _name; // повертає ім'я
-            set
-            {
-                // перевірка на пусте значення
-                if (string.IsNullOrWhiteSpace(value))
-                    throw new ArgumentException("Iм'я не може бути порожнiм.");
-
-                _name = value;
-            }
-        }
-
-        // Рівень доступний тільки для читання
+        public string Name { get; private set; }
         public int Level { get; private set; }
+        public int Experience { get; private set; }
 
-        // Досвід з автоматичним перерахунком рівня
-        public int Experience
-        {
-            get => _experience;
-            private set
-            {
-                if (value < 0)
-                    throw new ArgumentException("Досвiд не може бути негативним.");
-
-                _experience = value;
-
-                // Формула рівня
-                Level = _experience / XP_PER_LEVEL + 1;
-            }
-        }
-
-        // Конструктор
         public Player(string name)
         {
             Name = name;
-            Experience = 0; // стартовий досвід
+            Level = 1;
+            Experience = 0;
         }
 
-        // Метод додавання досвіду
-        public void AddExperience(int xp)
+        public void AddExperience(int amount)
         {
-            if (xp > 0)
-                Experience += xp;
+            Experience += amount;
+
+            while (Experience >= Level * 100)
+            {
+                Experience -= Level * 100;
+                Level++;
+            }
         }
 
-        // Відсоток до наступного рівня
         public int GetProgressPercent()
         {
-            return (Experience % XP_PER_LEVEL) * 100 / XP_PER_LEVEL;
+            return (int)((double)Experience / (Level * 100) * 100);
         }
     }
 }
