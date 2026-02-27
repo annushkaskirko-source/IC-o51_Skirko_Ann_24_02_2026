5bc9d2ce183c00ae68e18121c9ee481659bb2fb4
annushkaskirko-source <annushkaskirko@gmail.com>
2026-02-27 12:24:17 +0200

    Update GameManager.cs

--- IC-o51_Skirko_Ann_08_02_2026/Models/GameManager.cs
+++ IC-o51_Skirko_Ann_08_02_2026/Models/GameManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +6,11 @@ using System.Threading.Tasks;
 
 namespace IC_o51_Skirko_Ann_08_02_2026.Models
 {
-    // Патерн Singleton
     public class GameManager
     {
         private static GameManager _instance;
 
-        // Єдиний екземпляр класу
+        // Singleton Instance
         public static GameManager Instance
         {
             get
@@ -23,30 +22,79 @@ namespace IC_o51_Skirko_Ann_08_02_2026.Models
             }
         }
 
-        public Player Player { get; private set; } // гравець
-        public List<Quest> Quests { get; private set; } // список квестів
+        public Player Player { get; private set; }
+
+        // Використовуємо репозиторій замість List<Quest>
+        private IRepository<Quest> _questRepository;
 
         // Приватний конструктор
         private GameManager()
         {
-            Player = new Player("Anna"); // створення гравця
-            Quests = new List<Quest>();  // створення списку
+            Player = new Player("Anna");
+            _questRepository = new QuestRepository();
         }
 
         // Додати квест
         public void AddQuest(Quest quest)
         {
-            Quests.Add(quest);
+            if (quest == null)
+                throw new ArgumentNullException(nameof(quest));
+
+            _questRepository.Add(quest);
+        }
+
+        // Видалити квест
+        public bool RemoveQuest(Quest quest)
+        {
+            return _questRepository.Remove(quest);
         }
 
         // Завершити квест
         public void CompleteQuest(Quest quest)
         {
+            if (quest == null)
+                return;
+
             if (quest.Status == QuestStatus.Active)
             {
-                quest.Complete(); // змінюємо статус
-                Player.AddExperience(quest.ExperienceReward); // додаємо XP
+                quest.Complete();
+                _questRepository.Update(quest); // оновлюємо в репозиторії
+                Player.AddExperience(quest.ExperienceReward);
             }
+        }
+
+        // Отримати всі активні квести
+        public IEnumerable<Quest> GetActiveQuests()
+        {
+            return ((QuestRepository)_questRepository).GetActiveQuests();
+        }
+
+        // Отримати всі виконані квести
+        public IEnumerable<Quest> GetCompletedQuests()
+        {
+            return ((QuestRepository)_questRepository).GetCompletedQuests();
+        }
+
+        // Отримати квести за категорією
+        public IEnumerable<Quest> GetQuestsByCategory(QuestCategory category)
+        {
+            return ((QuestRepository)_questRepository).GetQuestsByCategory(category);
+        }
+
+        // Статистика
+        public int GetTotalQuestsCount()
+        {
+            return _questRepository.Count;
+        }
+
+        public int GetActiveQuestsCount()
+        {
+            return GetActiveQuests().Count();
+        }
+
+        public int GetCompletedQuestsCount()
+        {
+            return GetCompletedQuests().Count();
         }
     }
 }
