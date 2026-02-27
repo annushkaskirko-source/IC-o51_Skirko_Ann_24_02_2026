309dff593d3ea1e1f1e6a48a9a5f9ca1eca94ba0
annushkaskirko-source <annushkaskirko@gmail.com>
2026-02-27 12:22:53 +0200

    Create Enums.cs

---
+++ IC-o51_Skirko_Ann_08_02_2026/Models/Enums.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IC_o51_Skirko_Ann_08_02_2026
+{
+    public enum QuestStatus
+    {
+        Active,
+        Completed
+    }
+
+    public enum QuestCategory
+    {
+        Study,
+        Work,
+        Health,
+        Personal
+    }
+
+    public enum QuestDifficulty
+    {
+        Easy,
+        Medium,
+        Hard
+    }
+}
